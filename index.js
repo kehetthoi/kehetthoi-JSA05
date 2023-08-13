f3c3aec9718d26bbe4e74ed5237edd6664320e1d
kehetthoi <vohuy.76.thcsnvb2122@gmail.com>
2023-08-13 12:07:05 +0700

    buoi2

---
+++ index.js
@@ -0,0 +1,26 @@
+// let x = 0;
+// while(x<10){
+//    alert(x+=1); 
+// }
+// let x = 0;
+// do{
+// alert(x+=1)
+// }while(x<10)
+// for(var i=0;i<=10;i++){alert(i)}
+let s = [0,1,2];
+  let t = ["hieu","tai","ngoc"];
+  let x = 0;
+//  alert(t[0])
+
+//  alert(t[1])
+
+//  alert(t[2])
+
+//  alert(t[3])
+
+// {alert(i[1])}
+// {alert(i[2])}
+while(x<=2){
+    console.log(x +":"+ t[x]);
+    x++;
+}
