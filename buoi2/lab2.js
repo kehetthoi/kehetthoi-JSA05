a3e5f1fcff3f8217d42ab2ba0dd14007c3bddc5e
kehetthoi <vohuy.76.thcsnvb2122@gmail.com>
2023-08-20 11:31:51 +0700

    gui

---
+++ buoi2/lab2.js
@@ -0,0 +1,70 @@
+//let traicay=["Grapes",15];
+//let i=0;
+//while(i<=1)
+//{
+  //  console.log("I have " + traicay[1]+ " "+traicay[0]);
+   // i++;
+    
+//}
+
+//do{
+    //console.log("I have"+" "+ traicay[1]+" "+traicay[0]);
+   // i++;
+
+//}while(i<=1)
+//for(var i=0;i<=0;i++){
+   // console.log("I have"+" "+ traicay[1]+" "+traicay[0]);
+//}
+// var chocuatui={
+//     "name":"ngọc",
+//    "chân":"4",
+//     "color":"brown"
+
+// };
+// let myarry=[4,5,7,8,9,1]
+// console.log(myarry)
+// for(let i = 0;i < myarry.length;i++){
+//    console.log("gia tri cua bieu thuc",i,"la",myarry[i])
+// }
+// myarry[2]=6;
+// console.log(myarry)
+// myarry.splice(2,1,7,8);
+// console.log(myarry)
+// console.log(myarry.length)
+// myarry.push(7)
+// console.log(myarry)
+// myarry.pop()
+// console.log(myarry)
+// myarry.unshift(0)
+// console.log(myarry)
+// myarry.shift()
+// console.log(myarry)
+// chocuatui.name="tai"
+// chocuatui.action="chay"
+// delete(dog.action)
+// console.log(chocuatui)
+// let meo = new Object({
+//    name:"mai",
+//    age:3,
+//    enemy:{
+//       name:"ngoc",
+//       age:5
+//    }
+// })
+// console.log(meo)
+// let dog1 =["ngoc",4]
+// let dog2 =["tai",5]
+//  let dogs = [dog1,dog2]
+//  console.log(dogs)
+//  console.log(dogs[1][0])
+let giotraicay = [
+   ["xoai",2],
+   ["dua hau",3]
+]
+let i=0;
+while(i<giotraicay.length){
+   console.log(
+   "gio trai cay co"+" "+giotraicay[i] [0]+giotraicay[i] [1])
+   i++;
+
+}
